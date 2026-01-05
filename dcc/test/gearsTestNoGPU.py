6368e20d74792d548cec63ce2fdbef3e754efe56
Marc Duby <mduby@broadinstitute.org>
2026-01-05 14:27:19 -0500

    adding test script

---
+++ dcc/test/gearsTestNoGPU.py
@@ -0,0 +1,35 @@
+
+
+
+# imports
+import sys
+sys.path.append('/home/javaprog/Code/MlWorkspace/Dig-GearsGNN/')
+from gears import PertData
+
+
+
+# constants
+DIR_GEARS = "/home/javaprog/Data/Broad/GeneticsML/Gears202512"
+
+
+pert_data = PertData('{}/Data'.format(DIR_GEARS)) # specific saved folder
+pert_data.load(data_name = 'norman') # specific dataset name
+pert_data.prepare_split(split = 'simulation', seed = 1) # get data split with seed
+pert_data.get_dataloader(batch_size = 32, test_batch_size = 128) # prepare data loader
+
+# # set up and train a model
+# gears_model = GEARS(pert_data, device = 'cuda:8')
+# gears_model.model_initialize(hidden_size = 64)
+# gears_model.train(epochs = 20)
+
+# # save/load model
+# gears_model.save_model('gears')
+# gears_model.load_pretrained('gears')
+
+# # predict
+# gears_model.predict([['CBL', 'CNN1'], ['FEV']])
+# gears_model.GI_predict(['CBL', 'CNN1'], GI_genes_file=None)
+
+
+
+
